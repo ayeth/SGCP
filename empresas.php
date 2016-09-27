1edf63be31f99d6351b28836e093e29150f2d07f
Renan Mesquita <renanmesquita@outlook.com>
2016-09-27 13:44:44 -0300

    Removido tabs e espaços em excesso

--- empresas.php
+++ empresas.php
@@ -1,16 +1,12 @@
 <?php
-
-	
 	include 'conn.php';
+	mysql_set_charset('utf8');
 	$rs = mysql_query("select nome_emp from emp_sgp");
 	$items = array();
 	while($row = mysql_fetch_object($rs)){
 		array_push($items, $row);
 	}
 	$result = $items;
-	
-	
 
 	echo json_encode($result);
-
 ?>
