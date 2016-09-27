6dddbc15f7a8a6eaf919660c0661149d51e84620
Renan Mesquita <renanmesquita@outlook.com>
2016-09-27 13:43:33 -0300

    Atualizado comando SQL
    SELECT atualizado.

--- areas.php
+++ areas.php
@@ -1,7 +1,7 @@
 <?php
 	include 'conn.php';
 	mysql_set_charset('utf8');
-	$rs = mysql_query("select nome_area from area_sgp where nome_emp = $empresa_processo ORDER BY nome_area ASC");
+	$rs = mysql_query("select nome_area from area_sgp");
 	$items = array();
 	while($row = mysql_fetch_object($rs)){
 		array_push($items, $row);
