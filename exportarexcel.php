bb2505a4597607a071368aa597d8a509c4a56da0
Renan Mesquita <renanmesquita@outlook.com>
2016-10-14 16:00:42 -0300

    Update exportarexcel.php

--- exportarexcel.php
+++ exportarexcel.php
@@ -1,4 +1,5 @@
 <?php
+   mysql_set_charset('utf8');
    //Incluir a classe excelwriter
    include("excelwriter.inc.php");
 
@@ -10,7 +11,7 @@
    }
 
    //Escreve o nome dos campos de uma tabela
-   $myArr=array('STATUS','CODIGO','NOME','EMPRESA','AREA','VERSAO','DATA DE VENCIMENTO','LINK DO PROCESSO');
+   $myArr=array('STATUS','CÓDIGO','NOME','EMPRESA','ÁREA','VERSÃO','DATA DE VENCIMENTO','LINK DO PROCESSO','DATA DE PUBLICAÇÃO','DATA DE APROVAÇÃO','DATA DE CRIAÇÃO','DONO DO PROCESSO','OBSERVAÇÕES');
    $excel->writeLine($myArr);
 
    //Seleciona os campos de uma tabela
@@ -19,11 +20,11 @@
 	{
 	mysql_select_db("rkqvvexb_cip", $conn);
 	}
-   $consulta = "SELECT *  FROM id_processos";
+   $consulta = "SELECT *, DATE_FORMAT(vencimento_processo,'%d/%m/%Y') AS vencimento_processo, DATE_FORMAT(dt_criacao,'%d/%m/%Y') AS dt_criacao, DATE_FORMAT(dt_publicacao,'%d/%m/%Y') AS dt_publicacao, DATE_FORMAT(dt_aprovacao,'%d/%m/%Y') AS dt_aprovacao FROM id_processos";
    $resultado = mysql_query($consulta);
    if($resultado==true){
       while($linha = mysql_fetch_array($resultado)){ 
-         $myArr=array($linha['status_servico'],$linha['codigo_processo'],$linha['nome_processo'],$linha['empresa_processo'],$linha['area_processo'],$linha['VERSAO'],$linha['vencimento_processo'],$linha['bizagi_process_link'],$linha['dt_publicacao'],$linha['dt_aprovacao'],$linha['dt_criacao'],$linha['dt_criacao'],$linha['id_owner'],$linha['analista_processo'],$linha['obs_servico']);
+         $myArr=array($linha['status_servico'],$linha['codigo_processo'],$linha['nome_processo'],$linha['empresa_processo'],$linha['area_processo'],$linha['versao_processo'],$linha['vencimento_processo'],$linha['bizagi_process_link'],$linha['dt_publicacao'],$linha['dt_aprovacao'],$linha['dt_criacao'],$linha['dt_criacao'],$linha['id_owner'],$linha['obs_servico']);
 
          $excel->writeLine($myArr);
       }
@@ -31,6 +32,6 @@
 
 
     $excel->close();
-    echo "O arquivo foi gerado com sucesso. <a href=\"RelatorioSGP.xls\">RelatorioSGP.xls</a>";
+    echo "O arquivo foi gerado com sucesso. <a href=\"RelatorioSGP.xls\">Clique Aqui!</a>";
 
 ?>
